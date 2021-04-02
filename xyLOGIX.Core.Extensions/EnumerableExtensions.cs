1fd4665a9a8940dfdda6e4f5be05ab7c58a00c4f
Brian Hart <brian.hart@xylogix.net>
2021-04-02 16:27:38 -0400

    Move methods and consolidate code

--- xyLOGIX.Core.Extensions/EnumerableExtensions.cs
+++ xyLOGIX.Core.Extensions/EnumerableExtensions.cs
@@ -10,9 +10,13 @@ namespace xyLOGIX.Core.Extensions
     public static class EnumerableExtensions
     {
         /// <summary>
-        /// Compares the <paramref name="source"/> object with the <paramref
-        /// name="testObjects"/> provided, to see if any of the <paramref
-        /// name="testObjects"/> is a match.
+        /// Compares the <paramref name="source" /> object with the
+        /// <paramref
+        ///     name="testObjects" />
+        /// provided, to see if any of the
+        /// <paramref
+        ///     name="testObjects" />
+        /// is a match.
         /// </summary>
         /// <typeparam name="T">
         /// Type of the object to be tested.
@@ -21,17 +25,17 @@ namespace xyLOGIX.Core.Extensions
         /// Source object to check.
         /// </param>
         /// <param name="testObjects">
-        /// Object or objects that should be compared to source with the <see
-        /// cref="M:System.Object.Equals"/> method.
+        /// Object or objects that should be compared to source with the
+        /// <see
+        ///     cref="M:System.Object.Equals" />
+        /// method.
         /// </param>
         /// <returns>
-        /// True if any of the <paramref name="testObjects"/> equals the source;
+        /// True if any of the <paramref name="testObjects" /> equals the source;
         /// false otherwise.
         /// </returns>
         public static bool IsAnyOf<T>(this T source, params T[] testObjects)
-        {
-            return testObjects.Any(o => o.Equals(source));
-        }
+            => testObjects.Any(o => o.Equals(source));
 
         /// <summary>
         /// Shuffles the elements of the sequence into a random order, and then
@@ -48,9 +52,12 @@ namespace xyLOGIX.Core.Extensions
         /// to the Fisher-Yates-Durstenfeld shuffle.
         /// </returns>
         /// <remarks>
-        /// see, e.g., <a
-        /// href="http://stackoverflow.com/questions/5807128/an-extension-method-on-ienumerable-needed-for-shuffling">this
-        /// StackOverflow article.</a>
+        /// see, e.g.,
+        /// <a
+        ///     href="http://stackoverflow.com/questions/5807128/an-extension-method-on-ienumerable-needed-for-shuffling">
+        /// this
+        /// StackOverflow article.
+        /// </a>
         /// </remarks>
         public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source)
         {
@@ -59,10 +66,47 @@ namespace xyLOGIX.Core.Extensions
         }
 
         /// <summary>
+        /// Returns all the elements of the <paramref name="source" /> enumerable
+        /// object, except for the last.
+        /// </summary>
+        /// <typeparam name="T">
+        /// Name of the type of item in the collection.
+        /// </typeparam>
+        /// <param name="source">
+        /// Reference to an enumerable collection.
+        /// </param>
+        /// <returns>
+        /// Enumerable iterator over the collection that yields every item in
+        /// the collection, except for the last.
+        /// </returns>
+        public static IEnumerable<T> TakeAllButLast<T>(
+            this IEnumerable<T> source)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            using (var it = source.GetEnumerator())
+            {
+                var hasRemainingItems = false;
+                var isFirst = true;
+                var item = default(T);
+
+                do
+                {
+                    hasRemainingItems = it.MoveNext();
+                    if (!hasRemainingItems) continue;
+                    if (!isFirst) yield return item;
+                    item = it.Current;
+                    isFirst = false;
+                } while (hasRemainingItems);
+            }
+        }
+
+        /// <summary>
         /// Shuffles the elements of the sequence into a random order, and then
         /// returns the resulting sequence. From the Fisher-Yates-Durstenfeld
-        /// shuffle. Uses the random-number generator passed in the <paramref
-        /// name="rng"/> parameter.
+        /// shuffle. Uses the random-number generator passed in the
+        /// <paramref
+        ///     name="rng" />
+        /// parameter.
         /// </summary>
         /// <typeparam name="T">
         /// Type of the elements of the sequence.
@@ -71,7 +115,7 @@ namespace xyLOGIX.Core.Extensions
         /// Sequence to be shuffled.
         /// </param>
         /// <param name="rng">
-        /// A <see cref="T:System.Random"/> instance with which to generate new
+        /// A <see cref="T:System.Random" /> instance with which to generate new
         /// random values for the shuffle operation.
         /// </param>
         /// <returns>
@@ -79,9 +123,12 @@ namespace xyLOGIX.Core.Extensions
         /// to the Fisher-Yates-Durstenfeld shuffle.
         /// </returns>
         /// <remarks>
-        /// see, e.g., <a
-        /// href="http://stackoverflow.com/questions/5807128/an-extension-method-on-ienumerable-needed-for-shuffling">this
-        /// StackOverflow article.</a>
+        /// see, e.g.,
+        /// <a
+        ///     href="http://stackoverflow.com/questions/5807128/an-extension-method-on-ienumerable-needed-for-shuffling">
+        /// this
+        /// StackOverflow article.
+        /// </a>
         /// </remarks>
         private static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source,
             Random rng)
@@ -95,9 +142,12 @@ namespace xyLOGIX.Core.Extensions
         /// <summary>
         /// Shuffles the elements of the sequence into a random order, and then
         /// returns the resulting sequence. From the Fisher-Yates-Durstenfeld
-        /// shuffle. Uses the random-number generator passed in the <paramref
-        /// name="rng"/> parameter. Behaves like the <see
-        /// cref="M:xyLOGIX.Core.Extensions.EnumerableExtensions.Shuffle"/>
+        /// shuffle. Uses the random-number generator passed in the
+        /// <paramref
+        ///     name="rng" />
+        /// parameter. Behaves like the
+        /// <see
+        ///     cref="M:xyLOGIX.Core.Extensions.EnumerableExtensions.Shuffle" />
         /// method, although this method can be utilized as an iterator.
         /// </summary>
         /// <typeparam name="T">
@@ -107,7 +157,7 @@ namespace xyLOGIX.Core.Extensions
         /// Sequence to be shuffled.
         /// </param>
         /// <param name="rng">
-        /// A <see cref="T:System.Random"/> instance with which to generate new
+        /// A <see cref="T:System.Random" /> instance with which to generate new
         /// random values for the shuffle operation.
         /// </param>
         /// <returns>
@@ -115,9 +165,12 @@ namespace xyLOGIX.Core.Extensions
         /// to the Fisher-Yates-Durstenfeld shuffle.
         /// </returns>
         /// <remarks>
-        /// see, e.g., <a
-        /// href="http://stackoverflow.com/questions/5807128/an-extension-method-on-ienumerable-needed-for-shuffling">this
-        /// StackOverflow article.</a>
+        /// see, e.g.,
+        /// <a
+        ///     href="http://stackoverflow.com/questions/5807128/an-extension-method-on-ienumerable-needed-for-shuffling">
+        /// this
+        /// StackOverflow article.
+        /// </a>
         /// </remarks>
         private static IEnumerable<T> ShuffleIterator<T>(
             this IEnumerable<T> source, Random rng)

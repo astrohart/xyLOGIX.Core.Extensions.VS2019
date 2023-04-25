3a88fa51ebc7dbf468ff8d487d3ed0f05ae23056
Brian Hart <brian.hart@xylogix.net>
2023-04-25 08:52:50 -0600

    Create ControlFormAssociationProvider.cs

---
+++ xyLOGIX.Core.Extensions.Providers/ControlFormAssociationProvider.cs
@@ -0,0 +1,7 @@
+﻿namespace xyLOGIX.Core.Extensions.Providers
+{
+    /// <summary>
+    /// Defines associations between forms and their contained controls.
+    /// </summary>
+    public class ControlFormAssociationProvider { }
+}

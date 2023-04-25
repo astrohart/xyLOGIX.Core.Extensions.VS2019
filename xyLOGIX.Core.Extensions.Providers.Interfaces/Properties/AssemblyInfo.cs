9cc8a6bf0f1a5573b70f3db5dbd1dd4c43fea7c5
Brian Hart <brian.hart@xylogix.net>
2023-04-25 08:51:33 -0600

    Create xyLOGIX.Core.Extensions.Providers module

---
+++ xyLOGIX.Core.Extensions.Providers.Interfaces/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("xyLOGIX Interfaces Support Library for Core Extension Library's Providers of Control-Form Associations, Among Others Component")]
+[assembly: AssemblyDescription("Provides objects and functionality to provide access to data and UI element associations.  This particular library holds the C# interfaces for this component.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("xyLOGIX, LLC")]
+[assembly: AssemblyProduct("xyLOGIX Support Library for Core Extension Library")]
+[assembly:
+    AssemblyCopyright(
+        "Copyright © 2019-23 by xyLOGIX, LLC.  All rights reserved.")]
+[assembly:
+    AssemblyTrademark(
+        "This software is a component of xyLOGIX Core Extension Library.  xyLOGIX Core Extension Library is a trademark of xyLOGIX, LLC.  All rights reserved, especially those granted by international treaties.")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cb7a0864-159a-4744-b575-baa876ccd779")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.*")]

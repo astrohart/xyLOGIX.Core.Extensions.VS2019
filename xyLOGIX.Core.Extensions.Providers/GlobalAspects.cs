71f23d36396339d9f5cb612379c8a6726082695c
Brian Hart <brian.hart@xylogix.net>
2023-06-15 08:55:35 -0600

    Latest updates 08:55 06/15/2023

--- xyLOGIX.Core.Extensions.Providers/GlobalAspects.cs
+++ xyLOGIX.Core.Extensions.Providers/GlobalAspects.cs
@@ -4,6 +4,7 @@ using PostSharp.Patterns.Diagnostics;
 // This file contains registration of aspects that are applied to several classes of this project.
 [assembly:
     Log(
+        AttributePriority = 1,
         AttributeTargetTypeAttributes = MulticastAttributes.Private |
                                         MulticastAttributes.Protected |
                                         MulticastAttributes.Internal |
@@ -14,7 +15,6 @@ using PostSharp.Patterns.Diagnostics;
                                           MulticastAttributes.Public |
                                           MulticastAttributes.UserGenerated
     )]
-[assembly: Log(AttributePriority = 1)]
 [assembly: Log(AttributePriority = 2, AttributeExclude = true, AttributeTargetMembers = "get_*" )]
 [assembly: Log(AttributePriority = 3, AttributeExclude = true, AttributeTargetMembers = "set_*" )]
 [assembly: Log(AttributePriority = 4, AttributeExclude = true, AttributeTargetMembers = "add_*" )]

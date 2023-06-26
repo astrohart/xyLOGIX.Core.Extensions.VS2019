60bac85f60415e933f89a80f920b335684f6b524
Brian Hart <brian.hart@xylogix.net>
2023-06-26 16:48:20 -0600

    Latest updates

--- xyLOGIX.Core.Extensions/Properties/AssemblyInfo.cs
+++ xyLOGIX.Core.Extensions/Properties/AssemblyInfo.cs
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 // Major Version Minor Version Build Number Revision
 //
 // You can specify all the values or you can default the Build and Revision
-// Numbers by using the '*' as shown below: [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.*")]
+// Numbers by using the '*' as shown below: [assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]

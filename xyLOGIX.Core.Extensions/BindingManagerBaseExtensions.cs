59893c43d96497ff3959af0f056783d156779e05
Brian Hart <brian.hart@xylogix.net>
2023-11-03 17:57:52 -0600

    Latest updates at 17:57 hours MDT on 11/03/2023

---
+++ xyLOGIX.Core.Extensions/BindingManagerBaseExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows.Forms;
+using xyLOGIX.Core.Debug;
+
+namespace xyLOGIX.Core.Extensions
+{
+    public static class BindingManagerBaseExtensions
+    {
+        public static void SuspendTwoWayBinding(
+            this BindingManagerBase bindingManager
+        )
+        {
+            try
+            {
+                if (bindingManager == null) return;
+
+                foreach (Binding b in bindingManager.Bindings)
+                    b.DataSourceUpdateMode = DataSourceUpdateMode.Never;
+            }
+            catch (Exception ex)
+            {
+                // dump all the exception info to the log
+                DebugUtils.LogException(ex);
+            }
+        }
+
+        public static void UpdateDataBoundObject(
+            this BindingManagerBase bindingManager
+        )
+        {
+            try
+            {
+                if (bindingManager == null) return;
+
+                foreach (Binding b in bindingManager.Bindings)
+                {
+                    b.DataSourceUpdateMode =
+                        DataSourceUpdateMode.OnPropertyChanged;
+                    b.WriteValue();
+                }
+            }
+            catch (Exception ex)
+            {
+                // dump all the exception info to the log
+                DebugUtils.LogException(ex);
+            }
+        }
+    }
+}

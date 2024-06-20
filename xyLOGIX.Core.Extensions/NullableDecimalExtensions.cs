5d5bb5b0233e7133f103d8287200e42c81ef23de
Brian Hart <brian.hart@xylogix.net>
2024-06-20 17:41:08 -0600

    Update NullableDecimalExtensions.cs
    * Added a ToNearestCent() method.

--- xyLOGIX.Core.Extensions/NullableDecimalExtensions.cs
+++ xyLOGIX.Core.Extensions/NullableDecimalExtensions.cs
@@ -1,4 +1,7 @@
-﻿namespace xyLOGIX.Core.Extensions
+﻿using System;
+using xyLOGIX.Core.Debug;
+
+namespace xyLOGIX.Core.Extensions
 {
     /// <summary> Extension methods for a <c>decimal?</c> value. </summary>
     public static class NullableDecimalExtensions
@@ -74,5 +77,46 @@
         /// </returns>
         public static bool IsValid(this decimal? value)
             => value != null && value.HasValue;
+
+        /// <summary>
+        /// Rounds the specified <paramref name="value" /> to the nearest cent, assuming
+        /// that the specified <paramref name="value" /> is denominated in
+        /// <c>U.S. Dollars (USD)</c>.
+        /// </summary>
+        /// <param name="value">
+        /// (Required.) The <see cref="T:System.Decimal" /> value that
+        /// is to be rounded.
+        /// </param>
+        /// <returns>
+        /// Result of the rounding, using banker's rounding; or
+        /// <see cref="F:System.Decimal.Zero" /> if not successful.
+        /// <para />
+        /// <see cref="F:System.Decimal.Zero" /> is also returned if
+        /// <see cref="F:System.Decimal.Zero" /> or less is passed as the argument of
+        /// <paramref name="value" />.
+        /// </returns>
+        public static decimal ToNearestCent(this decimal? value)
+        {
+            var result = decimal.Zero;
+
+            try
+            {
+                if (value == null) return result;
+                if (!value.HasValue) return result;
+                if (decimal.Zero.Equals(value)) return result;
+                if (value < decimal.Zero) return result;
+
+                result = Math.Round(value.Value, 2, MidpointRounding.ToEven);
+            }
+            catch (Exception ex)
+            {
+                // dump all the exception info to the log
+                DebugUtils.LogException(ex);
+
+                result = decimal.Zero;
+            }
+
+            return result;
+        }
     }
 }

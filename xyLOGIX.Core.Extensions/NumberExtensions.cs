a3bf80c299265c24558ab13a008e05b80e47f9d0
Brian Hart <brian.hart@xylogix.net>
2024-06-20 17:47:49 -0600

    Update NumberExtensions.cs
    * Added a ToNearestCent() method to round decimal values assumed to be amounts of money denominated in U.S. Dollars to the nearest cent.

--- xyLOGIX.Core.Extensions/NumberExtensions.cs
+++ xyLOGIX.Core.Extensions/NumberExtensions.cs
@@ -518,6 +518,45 @@ namespace xyLOGIX.Core.Extensions
             => value == decimal.Zero;
 
         /// <summary>
+        /// Rounds the specified <paramref name="value" /> to the nearest cent, assuming
+        /// that the specified <paramref name="value" /> is denominated in
+        /// <c>U.S. Dollars (USD)</c>.
+        /// </summary>
+        /// <param name="value">
+        /// (Required.) The <see cref="T:System.Decimal" /> value that
+        /// is to be rounded.
+        /// </param>
+        /// <returns>
+        /// Result of the rounding, using banker's rounding; or
+        /// <see cref="F:System.Decimal.Zero" /> if not successful.
+        /// <para />
+        /// <see cref="F:System.Decimal.Zero" /> is also returned if
+        /// <see cref="F:System.Decimal.Zero" /> or less is passed as the argument of
+        /// <paramref name="value" />.
+        /// </returns>
+        public static decimal ToNearestCent(this decimal value)
+        {
+            var result = decimal.Zero;
+
+            try
+            {
+                if (decimal.Zero.Equals(value)) return result;
+                if (value < decimal.Zero) return result;
+
+                result = Math.Round(value, 2, MidpointRounding.ToEven);
+            }
+            catch (Exception ex)
+            {
+                // dump all the exception info to the log
+                DebugUtils.LogException(ex);
+
+                result = decimal.Zero;
+            }
+
+            return result;
+        }
+
+        /// <summary>
         /// Takes a value of type decimal and truncates it to the specified
         /// number of places.
         /// </summary>

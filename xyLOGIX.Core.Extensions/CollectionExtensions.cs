b0d8f8c6c0ab91bd40279bb1cac4d09508e19176
Brian Hart <brian.hart@xylogix.net>
2024-11-06 16:33:00 -0700

    Update CollectionExtensions.cs

--- xyLOGIX.Core.Extensions/CollectionExtensions.cs
+++ xyLOGIX.Core.Extensions/CollectionExtensions.cs
@@ -1,7 +1,9 @@
 ﻿using PostSharp.Patterns.Diagnostics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using xyLOGIX.Core.Debug;
 
 namespace xyLOGIX.Core.Extensions
 {
@@ -55,6 +57,80 @@ namespace xyLOGIX.Core.Extensions
                 if (item == null) continue;
                 collection.Add(item);
             }
+        }
+
+        /// <summary>
+        /// Determines whether the <paramref name="predicate" /> returns
+        /// <see langword="true" /> for any of the specified <paramref name="values" />.
+        /// </summary>
+        /// <typeparam name="T">
+        /// (Required.) Data type of each element of the
+        /// <paramref name="values" /> collection.
+        /// </typeparam>
+        /// <param name="values">
+        /// (Required.) Collection of objects, each of whose elements
+        /// are to be passed to the specified <paramref name="predicate" /> until either it
+        /// returns <see langword="true" />, or if the entire collection is enumerated.
+        /// </param>
+        /// <param name="predicate">
+        /// (Required.) Predicate that tests each value in the
+        /// <paramref name="values" /> collection.
+        /// </param>
+        /// <remarks>
+        /// This method can make no guarantee about the order in which it evaluates the
+        /// <paramref name="predicate" /> on the elements of the <paramref name="values" />
+        /// collection.
+        /// <para />
+        /// This method enumerates the entire <paramref name="values" />
+        /// collection twice; once to determine if it contains any elements, and the other
+        /// to test each element with the specified <paramref name="predicate" />.
+        /// <para />
+        /// If you do not want that, then use the LINQ <c>Any</c> method on the
+        /// <paramref name="values" /> collection instead.
+        /// <para />
+        /// The approach of this method is designed to make it an asset where thread-safety
+        /// is more important than performance.
+        /// <para />
+        /// Iteration of the <paramref name="values" /> collection stops the moment that
+        /// either the <paramref name="predicate" /> returns <see langword="true" />, or if
+        /// the end of the <paramref name="values" /> collection is reached.
+        /// </remarks>
+        /// <returns>
+        /// <see langword="true" /> if any of the <paramref name="values" />
+        /// matches the supplied <paramref name="predicate" />; <see langword="false" />
+        /// otheriwse.
+        /// </returns>
+        public static bool AnyAre<T>(
+            this IEnumerable<T> values,
+            Func<T, bool> predicate
+        )
+        {
+            var result = false;
+
+            try
+            {
+                if (values == null) return result;
+                if (values.ToArray()
+                          .Length <= 0) return result;
+
+                foreach (var value in values.ToArray())
+                {
+                    if (value == null) continue;
+                    if (!predicate(value)) continue;
+
+                    result = true;
+                    break;
+                }
+            }
+            catch (Exception ex)
+            {
+                // dump all the exception info to the log
+                DebugUtils.LogException(ex);
+
+                result = false;
+            }
+
+            return result;
         }
 
         /// <summary>

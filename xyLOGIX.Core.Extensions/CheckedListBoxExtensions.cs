3fa9549bf8cd0b83349944d136786fa6280788c1
Brian Hart <brian.hart@xylogix.net>
2024-11-16 22:08:28 -0700

    Latest updates at 22:08 hours MDT on 11/16/2024

--- xyLOGIX.Core.Extensions/CheckedListBoxExtensions.cs
+++ xyLOGIX.Core.Extensions/CheckedListBoxExtensions.cs
@@ -1,9 +1,7 @@
 using PostSharp.Patterns.Diagnostics;
 using System;
 using System.Windows.Forms;
-using Core.Logging;
-using Core.Logging.Constants;
-using Core.Logging.Events;
+using xyLOGIX.Core.Debug;
 
 namespace xyLOGIX.Core.Extensions
 {

01ccbf2a774eff1b19937761b81be58101b67efd
Brian Hart <brian.hart@xylogix.net>
2025-01-06 15:11:49 -0700

    Add ToOrdinalString method to IntExtensions class
    Introduce ToOrdinalString method to convert int to ordinal string.
    Appends appropriate suffix ("st", "nd", "rd", "th") based on value.
    Throws ArgumentOutOfRangeException for values less than 1.
    Determines correct suffix by examining last digit and last two digits.

--- xyLOGIX.Core.Extensions/IntExtensions.cs
+++ xyLOGIX.Core.Extensions/IntExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using PostSharp.Patterns.Diagnostics;
+using System;
 using System.Linq;
 
 namespace xyLOGIX.Core.Extensions
@@ -7,6 +8,58 @@ namespace xyLOGIX.Core.Extensions
     [Log(AttributeExclude = true)]
     public static class IntExtensions
     {
+        /// <summary>
+        /// Converts an <see cref="T:System.Int32" /> value to its ordinal string representation,
+        /// appending the appropriate suffix (e.g., "st", "nd", "rd", "th") based on the value.
+        /// </summary>
+        /// <param name="value">
+        /// (Required.) An <see cref="T:System.Int32" /> value that represents the number to format.
+        /// Must be 1 or greater.
+        /// </param>
+        /// <returns>
+        /// If successful, a <see cref="T:System.String" /> containing the ordinal representation
+        /// of the provided <paramref name="value" />.
+        /// </returns>
+        /// <exception cref="T:System.ArgumentOutOfRangeException">
+        /// Thrown when the provided <paramref name="value" /> is less than 1.
+        /// </exception>
+        public static string ToOrdinalString(this int value)
+        {
+            if (value < 1)
+                throw new ArgumentOutOfRangeException(nameof(value), "Value must be 1 or greater.");
+
+            // Determine the suffix
+            int lastDigit = value % 10;
+            int lastTwoDigits = value % 100;
+
+            string suffix;
+            if (lastTwoDigits >= 11 && lastTwoDigits <= 13)
+            {
+                suffix = "th";
+            }
+            else
+            {
+                switch (lastDigit)
+                {
+                    case 1:
+                        suffix = "st";
+                        break;
+                    case 2:
+                        suffix = "nd";
+                        break;
+                    case 3:
+                        suffix = "rd";
+                        break;
+                    default:
+                        suffix = "th";
+                        break;
+                }
+            }
+
+            // Return the formatted value
+            return $"{value}{suffix}";
+        }
+
         /// <summary>
         /// Returns a value indicating whether the specified
         /// <paramref name="value" /> is equal to one of the items in a collection of

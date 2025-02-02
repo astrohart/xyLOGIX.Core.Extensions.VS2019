d6d33d6b5517b4aa03b1f4ba1d823c6494b5812f
Brian Hart <brian.hart@xylogix.net>
2025-02-02 15:07:54 -0700

    Update PathnameExtensions.cs
    * Swapped functionality of HasExtension and FileExists methods
    * Made HasExtension public and updated to check file extension
    * Made FileExists private and updated to check file existence
    * Updated XML documentation comments for both methods
    * HasExtension returns false for null/blank parameters or non-existent file
    * FileExists returns false for null/blank pathname and uses File.Exists
    * Marked HasExtension with [NotLogged] attribute

--- xyLOGIX.Core.Extensions/PathnameExtensions.cs
+++ xyLOGIX.Core.Extensions/PathnameExtensions.cs
@@ -14,50 +14,32 @@ namespace xyLOGIX.Core.Extensions
     public static class PathnameExtensions
     {
         /// <summary>
-        /// Extension method that can be used with all
-        /// <see cref="T:System.String" /> variables to determine whether a particular
-        /// file's <paramref name="pathname" /> has a given filename
-        /// <paramref name="extension" />.
+        /// Determines whether the file having the specified
+        /// <paramref name="pathname" /> exists on the filesystem or not.
         /// </summary>
         /// <param name="pathname">
         /// (Required.) A <see cref="T:System.String" /> containing
-        /// the fully-qualified pathname whose filename extension is to be tested.
-        /// </param>
-        /// <param name="extension">
-        /// (Required.) A <see cref="T:System.String" />
-        /// containing the filename extension that the specified
-        /// <paramref name="pathname" /> should have.
+        /// the fully-qualified pathname of the file to check for existence.
         /// </param>
         /// <returns>
         /// <see langword="true" /> if the specified <paramref name="pathname" />
-        /// has the specified filename <paramref name="extension" />;
-        /// <see langword="false" /> otherwise.
+        /// is not blank, and if it contains the fully-qualified pathname of a file that
+        /// currently exists on the filesystem, or <see langword="false" /> otherwise.
         /// </returns>
         /// <remarks>
-        /// If the <paramref name="pathname" /> or <paramref name="extension" />
-        /// have <see langword="null" /> or blank <see cref="T:System.String" /> values as
-        /// their arguments, then this method returns <see langword="false" />.
-        /// <para />
-        /// <see langword="false" /> is also returned if the argument of the
-        /// <paramref name="pathname" /> parameter refers to a file that does not exist on
-        /// the filesystem.
-        /// <para />
-        /// <b>NOTE:</b> This method is meant to be used as an extension method of type
-        /// <see cref="T:System.String" />.
+        /// This method is superior to calling the
+        /// <see cref="M:Alphaleonis.Win32.Filesystem.File.Exists" /> method.  This is due
+        /// to the fact that the other method throws an exception if it is passed a blank
+        /// string, whereas this method simply returns <see langword="false" />.
         /// </remarks>
-        public static bool HasExtension(
-            this string pathname,
-            string extension
-        )
+        private static bool FileExists(string pathname)
         {
-            var result = false;
+            bool result;
 
             try
             {
-                if (string.IsNullOrWhiteSpace(pathname)) return result;
-                if (string.IsNullOrWhiteSpace(extension)) return result;
-
-                result = extension.Equals(Path.GetExtension(pathname));
+                result = !string.IsNullOrWhiteSpace(pathname) &&
+                         File.Exists(pathname);
             }
             catch (Exception ex)
             {
@@ -129,32 +111,50 @@ namespace xyLOGIX.Core.Extensions
         }
 
         /// <summary>
-        /// Determines whether the file having the specified
-        /// <paramref name="pathname" /> exists on the filesystem or not.
+        /// Extension method that can be used with all
+        /// <see cref="T:System.String" /> variables to determine whether a particular
+        /// file's <paramref name="pathname" /> has a given filename
+        /// <paramref name="extension" />.
         /// </summary>
         /// <param name="pathname">
         /// (Required.) A <see cref="T:System.String" /> containing
-        /// the fully-qualified pathname of the file to check for existence.
+        /// the fully-qualified pathname whose filename extension is to be tested.
+        /// </param>
+        /// <param name="extension">
+        /// (Required.) A <see cref="T:System.String" />
+        /// containing the filename extension that the specified
+        /// <paramref name="pathname" /> should have.
         /// </param>
         /// <returns>
         /// <see langword="true" /> if the specified <paramref name="pathname" />
-        /// is not blank, and if it contains the fully-qualified pathname of a file that
-        /// currently exists on the filesystem, or <see langword="false" /> otherwise.
+        /// has the specified filename <paramref name="extension" />;
+        /// <see langword="false" /> otherwise.
         /// </returns>
         /// <remarks>
-        /// This method is superior to calling the
-        /// <see cref="M:Alphaleonis.Win32.Filesystem.File.Exists" /> method.  This is due
-        /// to the fact that the other method throws an exception if it is passed a blank
-        /// string, whereas this method simply returns <see langword="false" />.
+        /// If the <paramref name="pathname" /> or <paramref name="extension" />
+        /// have <see langword="null" /> or blank <see cref="T:System.String" /> values as
+        /// their arguments, then this method returns <see langword="false" />.
+        /// <para />
+        /// <see langword="false" /> is also returned if the argument of the
+        /// <paramref name="pathname" /> parameter refers to a file that does not exist on
+        /// the filesystem.
+        /// <para />
+        /// <b>NOTE:</b> This method is meant to be used as an extension method of type
+        /// <see cref="T:System.String" />.
         /// </remarks>
-        private static bool FileExists(string pathname)
+        public static bool HasExtension(
+            [NotLogged] this string pathname,
+            string extension
+        )
         {
-            bool result;
+            var result = false;
 
             try
             {
-                result = !string.IsNullOrWhiteSpace(pathname) &&
-                         File.Exists(pathname);
+                if (string.IsNullOrWhiteSpace(pathname)) return result;
+                if (string.IsNullOrWhiteSpace(extension)) return result;
+
+                result = extension.Equals(Path.GetExtension(pathname));
             }
             catch (Exception ex)
             {

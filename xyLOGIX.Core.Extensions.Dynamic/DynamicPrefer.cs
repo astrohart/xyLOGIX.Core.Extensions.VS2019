c847a42d59b1939edb79d44d0a41192f97830657
Brian Hart <brian.hart@xylogix.net>
2025-02-19 12:55:12 -0700

    Add DynamicPrefer class and update project files
    * Added a new binary file `1382_cogs.ico`.
    * Created `DynamicPrefer.cs` with a static class for handling dynamic values and null checks, including logging attributes.
    * Updated `GlobalAspects.cs` to enhance logging capabilities across classes.
    * Modified `AssemblyInfo.cs` to include assembly metadata such as title, description, and copyright.
    * Added `Resources.Designer.cs` for managing localized strings and resources.
    * Created a new `Resources.resx` file for resource management structure.
    * Updated `README.md` to document the new `Resources` class and its properties.
    * Added `app.config` for assembly binding redirects, particularly for `log4net`.
    * Included a new binary file `key.snk` for assembly signing.
    * Updated `packages.config` to include new NuGet package dependencies like `AlphaFS`, `log4net`, `Newtonsoft.Json`, and various `PostSharp` packages.
    * Significantly updated `xyLOGIX.Core.Extensions.Dynamic.csproj` to reference new files, set build properties, and manage dependencies, including pre-build and post-build events for README generation and cleanup.

---
+++ xyLOGIX.Core.Extensions.Dynamic/DynamicPrefer.cs
@@ -0,0 +1,82 @@
+﻿using PostSharp.Patterns.Diagnostics;
+using System;
+using xyLOGIX.Core.Debug;
+
+namespace xyLOGIX.Core.Extensions.Dynamic
+{
+    /// <summary>
+    /// Contains methods that allow you to prefer one type over another when working
+    /// with <see langword="dynamic" /> objects.
+    /// </summary>
+    public static class DynamicPrefer
+    {
+        /// <summary>
+        /// Initializes static data or performs actions that need to be performed once only
+        /// for the <see cref="T:xyLOGIX.Core.Extensions.Dynamic.DynamicPrefer" /> class.
+        /// </summary>
+        /// <remarks>
+        /// This constructor is called automatically prior to the first instance being
+        /// created or before any static members are referenced.
+        /// <para />
+        /// We've decorated this constructor with the <c>[Log(AttributeExclude = true)]</c>
+        /// attribute in order to simplify the logging output.
+        /// </remarks>
+        [Log(AttributeExclude = true)]
+        static DynamicPrefer() { }
+
+        /// <summary>
+        /// Preferentially returns the specified <paramref name="preferredValue" /> over
+        /// the <paramref name="nullableValue" />, if the specified
+        /// <paramref name="nullableValue" /> is <see langword="null" /> or it does not
+        /// have a value; otherwise, the value of the <paramref name="nullableValue" /> is
+        /// returned.
+        /// </summary>
+        /// <param name="nullableValue">
+        /// (Required.) A <see cref="T:System.Object" /> variable whose value is to be
+        /// returned if it indeed does have a non-<see langword="null" />, non-blank value.
+        /// </param>
+        /// <param name="preferredValue">
+        /// (Required.) A <see cref="T:System.Object" />
+        /// containing the preferred value that is to be returned if the
+        /// <paramref name="nullableValue" /> is <see langword="null" /> or does not have a
+        /// value.
+        /// </param>
+        /// <returns>
+        /// The specified <paramref name="preferredValue" /> over the
+        /// <paramref name="nullableValue" />, if the specified
+        /// <paramref name="nullableValue" /> is <see langword="null" /> or it does not
+        /// have a value; otherwise, the value of the <paramref name="nullableValue" /> is
+        /// returned.
+        /// </returns>
+        [Log(AttributeExclude = true)]
+        [return: NotLogged]
+        public static dynamic DynamicOverNull(
+            [NotLogged] dynamic nullableValue,
+            [NotLogged] dynamic preferredValue
+        )
+        {
+            var result = preferredValue;
+
+            try
+            {
+                if (nullableValue == null && preferredValue == null)
+                    return result;
+                if (nullableValue == null && preferredValue != null)
+                    return result;
+                if (nullableValue != null && preferredValue == null)
+                    return nullableValue;
+
+                result = preferredValue;
+            }
+            catch (Exception ex)
+            {
+                // dump all the exception info to the log
+                DebugUtils.LogException(ex);
+
+                result = preferredValue;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ xyLOGIX.Core.Extensions.Dynamic/Properties/AssemblyInfo.cs
@@ -0,0 +1,44 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("xyLOGIX.Core.Extensions.Dynamic")]
+[assembly:
+    AssemblyDescription(
+        "Provides objects and functionality support to extend the default behavior of system objects and types.  This particular class library contains class(es), event(s), property(ies), and method(s) for working with dynamically-typed variables."
+    )]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("xyLOGIX, LLC")]
+[assembly: AssemblyProduct("xyLOGIX Core Extension Library")]
+[assembly:
+    AssemblyCopyright(
+        "Copyright © 2017-25 by xyLOGIX, LLC.  All rights reserved."
+    )]
+[assembly:
+    AssemblyTrademark(
+        "This software is a component of xyLOGIX Core Extension Library.  Core Extension Library is a trademark of xyLOGIX, LLC.  All rights reserved, especially those granted by international treaties.  Other brand names, brands, logos, service marks, trademarks, and registered trademarks herein belong to their respective owners."
+    )]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6095856e-802b-4bf8-853f-6b01a6bbb633")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

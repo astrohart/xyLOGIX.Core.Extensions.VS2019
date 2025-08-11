e0e3fce2b0be122ec9eaa61df101ffc4a9b8c0d8
Brian Hart <brian.hart@xylogix.net>
2025-08-11 07:20:14 -0600

    Create StringArrayExtensions.cs
    - Introduce a new static class `StringArrayExtensions`
      - Provides extension methods for string arrays
      - Includes a static constructor with logging attribute
      - Implements `DumpToLog` method for logging array contents
      - Handles null or empty arrays gracefully
    
    - Add `StringArrayExtensions.cs` to project file
      - Ensures it is compiled as part of the project

---
+++ xyLOGIX.Core.Extensions/StringArrayExtensions.cs
@@ -0,0 +1,62 @@
+﻿using PostSharp.Patterns.Diagnostics;
+using System;
+using System.Diagnostics;
+using xyLOGIX.Core.Debug;
+
+namespace xyLOGIX.Core.Extensions
+{
+    /// <summary>
+    /// Provides extension methods for <see cref="T:System.String[]" /> arrays.
+    /// </summary>
+    public static class StringArrayExtensions
+    {
+        /// <summary>
+        /// Initializes static data or performs actions that need to be performed once only
+        /// for the <see cref="T:xyLOGIX.Core.Extensions.StringArrayExtensions" /> class.
+        /// </summary>
+        /// <remarks>
+        /// This constructor is called automatically prior to the first instance being
+        /// created or before any static members are referenced.
+        /// <para />
+        /// We've decorated this constructor with the <c>[Log(AttributeExclude = true)]</c>
+        /// attribute in order to simplify the logging output.
+        /// </remarks>
+        [Log(AttributeExclude = true)]
+        static StringArrayExtensions() { }
+
+        /// <summary>
+        /// Dumps the contents of the specified <see cref="T:System.String[]" />
+        /// array to the application log.
+        /// </summary>
+        /// <param name="values">
+        /// (Required.) Reference to the <see cref="T:System.String[]" /> whose
+        /// contents are to be logged.
+        /// </param>
+        [DebuggerStepThrough]
+        public static void DumpToLog([NotLogged] this string[] values)
+        {
+            try
+            {
+                if (values == null) return;
+                if (values.Length <= 0) return;
+
+                DebugUtils.WriteLine(
+                    DebugLevel.Info,
+                    $"StringArrayExtensions.DumpToLog: Dumping {values.Length} value(s) to log..."
+                );
+
+                for (var i = 0; i < values.Length; i++)
+                {
+                    DebugUtils.WriteLine(
+                        DebugLevel.Debug, $"[{i}]: '{values[i]}'"
+                    );
+                }
+            }
+            catch (Exception ex)
+            {
+                // dump all the exception info to the log
+                DebugUtils.LogException(ex);
+            }
+        }
+    }
+}

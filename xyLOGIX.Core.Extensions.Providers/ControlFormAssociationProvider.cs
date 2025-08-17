7eaa82757f5e8e80f56c9f25f92fdd2b13811519
Brian Hart <brian.hart@xylogix.net>
2025-08-16 18:11:12 -0600

    Latest updates at 6:10 PM MDT on Sat 08/16/2025

--- xyLOGIX.Core.Extensions.Providers/ControlFormAssociationProvider.cs
+++ xyLOGIX.Core.Extensions.Providers/ControlFormAssociationProvider.cs
@@ -24,7 +24,7 @@ namespace xyLOGIX.Core.Extensions.Providers
         static ControlFormAssociationProvider() { }
 
         /// <summary>
-        /// Empty, protected constructor to prohibit direct allocation of this
+        /// Empty, <see langword="private" /> constructor to prohibit direct allocation of this
         /// class.
         /// </summary>
         [Log(AttributeExclude = true)]

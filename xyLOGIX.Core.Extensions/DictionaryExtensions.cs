0641ade35f8140b4b8b09433873bf340061aee24
Brian Hart <brian.hart@xylogix.net>
2025-09-22 00:21:37 -0600

    Update DictionaryExtensions.cs
    - Replacing all occurrences of `object sender` and `EventArgs e`, respectively, are prefaced with `[NotLogged]`.  This is being done so as to ensure that the value(s) of these parameter(s) do not get written to PostSharp log file(s).  Also updated all auto-property(ies) to make sure their getters and setters are all prefixed with the `[DebuggerStepThrough]` attribute.

--- xyLOGIX.Core.Extensions/DictionaryExtensions.cs
+++ xyLOGIX.Core.Extensions/DictionaryExtensions.cs
@@ -1,4 +1,5 @@
-﻿using PostSharp.Patterns.Diagnostics;
+﻿using System.Diagnostics;
+using PostSharp.Patterns.Diagnostics;
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;

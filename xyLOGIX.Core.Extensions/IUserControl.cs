daaa59442fdbe2dd72a0eb857eea96020e9d21fd
Brian Hart <brian.hart@xylogix.net>
2025-10-19 18:33:31 -0600

    Update IUserControl.cs
    - Adds a new interface `IUserControl` in the namespace `xyLOGIX.Core.Extensions`.
    - Defines a property `BorderStyle` of type `BorderStyle`.
    - Includes getter and setter methods marked with the `[DebuggerStepThrough]` attribute.
    - Adds necessary `using` directives for `System.Diagnostics` and `System.Windows.Forms`.

---
+++ xyLOGIX.Core.Extensions/IUserControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace xyLOGIX.Core.Extensions
+{
+    /// <summary>
+    /// Defines the publicly-exposed events, methods and properties of a control that
+    /// inherits <see cref="T:System.Windows.Forms.UserControl" />.
+    /// </summary>
+    public interface IUserControl
+    {
+        /// <summary>
+        /// Gets or sets the border style for the text box.
+        /// </summary>
+        /// <value>
+        /// A <see cref="T:System.Windows.Forms.BorderStyle" /> indicating the border
+        /// appearance.
+        /// </value>
+        BorderStyle BorderStyle
+        {
+            [DebuggerStepThrough] get;
+            [DebuggerStepThrough] set;
+        }
+    }
+}

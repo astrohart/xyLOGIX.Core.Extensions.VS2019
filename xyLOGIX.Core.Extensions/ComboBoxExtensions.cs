1dcd98859a2940bf9b68d23eb40d04bef6d6aaaf
Brian Hart <brian.hart@xylogix.net>
2025-11-08 13:45:26 -0700

    Update ComboBoxExtensions.cs
    - Removed all reference(s) to the `xyLOGIX.Files.Action` module, as it is being retired.  Also, reformatted the source file(s) in accordance with rules that are defined in CodeMaid and ReSharper.

--- xyLOGIX.Core.Extensions/ComboBoxExtensions.cs
+++ xyLOGIX.Core.Extensions/ComboBoxExtensions.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics;
-using PostSharp.Patterns.Collections;
+﻿using PostSharp.Patterns.Collections;
 using PostSharp.Patterns.Diagnostics;
 using System;
 using System.Collections.Generic;

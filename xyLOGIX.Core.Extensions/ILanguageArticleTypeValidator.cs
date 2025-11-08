7997865e4e8e37abce56772fa3103b78f1d9c7d9
Brian Hart <brian.hart@xylogix.net>
2025-11-08 13:45:29 -0700

    Update ILanguageArticleTypeValidator.cs
    - Removed all reference(s) to the `xyLOGIX.Files.Action` module, as it is being retired.  Also, reformatted the source file(s) in accordance with rules that are defined in CodeMaid and ReSharper.

--- xyLOGIX.Core.Extensions/ILanguageArticleTypeValidator.cs
+++ xyLOGIX.Core.Extensions/ILanguageArticleTypeValidator.cs
@@ -19,7 +19,8 @@
         /// </summary>
         /// <param name="type">
         /// (Required.) One of the
-        /// <see cref="T:xyLOGIX.Core.Extensions.LanguageArticleType" /> value(s) that is to
+        /// <see cref="T:xyLOGIX.Core.Extensions.LanguageArticleType" /> value(s) that is
+        /// to
         /// be examined.
         /// </param>
         /// <returns>

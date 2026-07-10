d5ba5872fd5c46d9fccd8f4187d41f02b80dc27d
Brian C. Hart, Ph.D. <brian.hart@xylogix.net>
2026-07-10 11:50:45 -0600

    Create UnsignedIntExtensions.cs
    - Added the `UnsignedIntExtensions` helper class with extension methods for `System.UInt32` values.
    - Implemented `EqualsOneOf` to test whether an unsigned integer matches any value in a supplied integer collection, with defensive exception logging.
    - Added the `IsBitmaskOn` extension method to simplify unsigned integer bitmask checks.
    - Implemented `ToOrdinalString` to format unsigned integers as ordinal strings with the correct English suffixes and argument validation.

---
+++ xyLOGIX.Core.Extensions/UnsignedIntExtensions.cs
@@ -0,0 +1,128 @@
+﻿using PostSharp.Patterns.Diagnostics;
+using System;
+using xyLOGIX.Core.Debug;
+
+namespace xyLOGIX.Core.Extensions
+{
+    /// <summary> Provides methods that extend the <see cref="T:System.UInt32" /> class. </summary>
+    [Log(AttributeExclude = true)]
+    public static class UnsignedIntExtensions
+    {
+        /// <summary>
+        /// Returns a value indicating whether the specified
+        /// <paramref name="value" /> is equal to one of the items in a collection of
+        /// integers.
+        /// </summary>
+        /// <param name="value"> Integer to be compared to the members of the list. </param>
+        /// <param name="list">
+        /// Collection of integers that should be searched for the
+        /// <paramref name="value" />.
+        /// </param>
+        /// <returns>
+        /// <see langword="true" /> if <paramref name="value" /> is one of the
+        /// elements of the collection; <see langword="false" /> if not.
+        /// </returns>
+        public static bool EqualsOneOf(
+            [NotLogged] this uint value,
+            [NotLogged] params int[] list
+        )
+        {
+            var result = false;
+
+            try
+            {
+                if (list == null) return result;
+                if (list.Length <= 0) return result;
+
+                foreach (var x in list)
+                {
+                    if (value != x) continue;
+
+                    result = true;
+                    break;
+                }
+            }
+            catch (Exception ex)
+            {
+                // dump all the exception info to the log
+                DebugUtils.LogException(ex);
+
+                result = false;
+            }
+
+            return result;
+        }
+
+        /// <summary> Determines whether a bitmask is 'on' or 'off'. </summary>
+        /// <param name="num">(Required.) Value containing the data to be checked.</param>
+        /// <param name="bitmask">(Required.) Bitmask.</param>
+        /// <returns>
+        /// <see langword="true" /> if the bit that is identified by the bitmask
+        /// in the data is 'on'; <see langword="false" /> if it is not.
+        /// </returns>
+        public static bool IsBitmaskOn(this uint num, uint bitmask)
+            => (num & bitmask) != 0;
+
+        /// <summary>
+        /// Converts an <see cref="T:System.UInt32" /> <paramref name="value" /> to
+        /// its ordinal string representation, appending the appropriate suffix (e.g.,
+        /// <c>"st"</c>, <c>"nd"</c>, <c>"rd"</c>, <c>"th"</c>, etc.) based on the
+        /// specified <paramref name="value" />.
+        /// </summary>
+        /// <param name="value">
+        /// (Required.) An <see cref="T:System.UInt32" /> value that
+        /// represents the number to format.
+        /// <para />
+        /// <b>NOTE:</b> The argument of this parameter must be <c>1</c> or greater.
+        /// </param>
+        /// <returns>
+        /// If successful, a <see cref="T:System.String" /> containing the ordinal
+        /// representation of the provided <paramref name="value" />.
+        /// </returns>
+        /// <exception cref="T:System.ArgumentOutOfRangeException">
+        /// Thrown when the provided
+        /// <paramref name="value" /> is less than 1.
+        /// </exception>
+        public static string ToOrdinalString(this uint value)
+        {
+            if (value < 1)
+                throw new ArgumentOutOfRangeException(
+                    nameof(value), "Value must be 1 or greater."
+                );
+
+            // Determine the suffix
+            var lastDigit = value % 10;
+            var lastTwoDigits = value % 100;
+
+            string suffix;
+            if (lastTwoDigits >= 11 && lastTwoDigits <= 13)
+            {
+                suffix = "th";
+            }
+            else
+            {
+                switch (lastDigit)
+                {
+                    case 1:
+                        suffix = "st";
+                        break;
+
+                    case 2:
+                        suffix = "nd";
+                        break;
+
+                    case 3:
+                        suffix = "rd";
+                        break;
+
+                    default:
+                        suffix = "th";
+                        break;
+                }
+            }
+
+            // Return the formatted value
+            return $"{value}{suffix}";
+        }
+    }
+}
